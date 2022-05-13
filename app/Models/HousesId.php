49ca7165fedc72d41f284590d5a88cfce5ec345e
Nor01 <mainor.aguilar@hotmail.com>
2022-05-12 23:21:58 -0600

    models schema created
    models schema created

---
+++ app/Models/HousesId.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HousesId extends Model
+{
+    use HasFactory;
+}
